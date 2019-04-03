d30e0d3cf834f2b79ba708f7f93a96e9213237fd
Roman Frolov <justcallmefrolov@gmail.com>
2019-04-03 12:46:35 +0200

    Added trial version of genesis library and unit test for it. (#22)
    - trial version of genesis library
    - genesis to file. 
    - tests

---
+++ test/unit/genesis.js
@@ -0,0 +1,77 @@
+/**
+ * @module test/unit/genesis
+ */
+
+'use strict';
+
+const chai = require('chai');
+chai.use(require('chai-like'));
+require('chai').should();
+
+const fs      = require('fs');
+const rb      = require('crypto').randomBytes;
+const Genesis = require('lib/genesis');
+
+describe('Create genesis', () => {
+    let genesis;
+    let address;
+    let amount;
+
+    before('initiate genesis', () => {
+        genesis = Genesis();
+    });
+
+    beforeEach('generate random address and amount', () => {
+        address = '0x' + rb(20).toString('hex');
+        amount  = Math.round(Math.random() * 100);
+    });
+
+    it('add block producer', () => {
+        genesis.addProducer(address, amount);
+
+        const allocObject = findAllocatedObject(genesis, address);
+
+        allocObject.certificates.length.should.be.equal(amount);
+        allocObject.balance.should.be.equal(0);
+    });
+
+    it('add delegate', () => {
+        genesis.addDelegate(address, amount);
+
+        const allocObject = findAllocatedObject(genesis, address);
+
+        allocObject.votes[0].should.be.equal(address);
+        allocObject.balance.should.be.equal(amount);
+    });
+
+    it('add account', () => {
+        genesis.addAccount(address, amount);
+
+        const allocObject = findAllocatedObject(genesis, address);
+
+        allocObject.balance.should.be.equal(amount);
+    });
+
+    it('save as file', () => {
+        const path = 'test/genesis.test.json';
+
+        genesis.writeToFile(path);
+
+        const genesisFromFile = require(path);
+
+        genesisFromFile.number.should.be.like(genesis.number);
+
+        fs.unlinkSync(path);
+    });
+});
+
+/**
+ * Find allocated object in genesis by account address.
+ *
+ * @param  {Genesis} genesis Object where to look for account.
+ * @param  {String}  address Address of account to find.
+ * @return {Object}          Found account.
+ */
+function findAllocatedObject(genesis, address) {
+    return (genesis.alloc.find(el => (Object.keys(el)[0] === address)))[address];
+}
